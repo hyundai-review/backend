632636c3233dcb929d486c230318565988e22dc6
jinii9 <jinii915@naver.com>
2024-11-05 05:03:00 +0900

    first commit

---
+++ src/test/java/hyundai/movie/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hyundai.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
