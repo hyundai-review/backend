52fd4bc0526bb6efa3e3bbe953f01a1c18e073cc
Haneol <haneolj@gmail.com>
2024-11-12 11:10:25 +0900

    [feature/login] feat: 스프링 시큐리티 설정 및 JWT 추가

--- src/main/java/hyundai/movie/global/auth/jwt/JwtTokenProvider.java
+++ src/main/java/hyundai/movie/global/auth/jwt/JwtTokenProvider.java
@@ -1,5 +1,86 @@
 package hyundai.movie.global.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.security.Key;
+import java.time.Duration;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration}")
+    private Duration accessTokenExpiration;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generate(Long memberId) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + accessTokenExpiration.toMillis());
+
+        return Jwts.builder()
+                .setSubject(memberId.toString())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+    public Long getMemberId(String token) {
+        Claims claims = parseClaims(token);
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("유효하지 않은 JWT signature.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료 된 JWT token.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT token.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT token의 형식이 유효하지 않음.");
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
 }
+
