da604e383ed6d0cd6a4c1e650ee296021a1dc46d
Haneol <haneolj@gmail.com>
2024-11-12 11:11:46 +0900

    [feature/login] feat: 소셜 로그인 구현

--- src/main/java/hyundai/movie/domains/member/repository/MemberRepository.java
+++ src/main/java/hyundai/movie/domains/member/repository/MemberRepository.java
@@ -1,5 +1,11 @@
 package hyundai.movie.domains.member.repository;
 
-public class MemberRepository {
+import hyundai.movie.domains.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByProviderId(String providerId);
 }
