65bdc1baef6c6a80e13f3fde4cb277931f331a15
Haneol <haneolj@gmail.com>
2024-11-14 00:17:57 +0900

    [feature/movie] feat: BoxOffice 도메인 추가

---
+++ src/main/java/hyundai/movie/domains/movie/domain/BoxOffice.java
@@ -0,0 +1,31 @@
+package hyundai.movie.domains.movie.domain;
+
+import hyundai.movie.global.common.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "boxoffice")
+public class BoxOffice extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    private Integer rank;
+
+    private String targetDate;
+}
--- src/main/java/hyundai/movie/domains/movie/service/MovieService.java
+++ src/main/java/hyundai/movie/domains/movie/service/MovieService.java
@@ -1,8 +1,10 @@
 package hyundai.movie.domains.movie.service;
 
+import hyundai.movie.domains.movie.api.response.BoxOfficeListResponse;
 import hyundai.movie.domains.movie.api.response.MovieResponse;
 import hyundai.movie.domains.movie.domain.Movie;
 import hyundai.movie.domains.movie.exception.MovieNotFoundException;
+import hyundai.movie.domains.movie.repository.BoxOfficeRepository;
 import hyundai.movie.domains.movie.repository.GenreRepository;
 import hyundai.movie.domains.movie.repository.MovieRepository;
 import jakarta.transaction.Transactional;
@@ -12,12 +14,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final BoxOfficeRepository boxOfficeRepository;
     private final GenreRepository genreRepository;
+
 
     @Transactional
     public MovieResponse getMovie(Long id) {
@@ -31,5 +35,20 @@ public class MovieService {
         return movieRepository.findAll().stream()
                 .map(MovieResponse::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public BoxOfficeListResponse getBoxOfficeMovieList() {
+        // 1. BoxOffice Entity를 뒤져, 오늘 날짜 박스오피스가 있는지 확인
+
+        // 2. 만약 있다면, 해당 정보를 리턴
+
+        // 3. 만약 없다면, KobisApiClient를 사용해 박스오피스 순위를 찾음
+
+        // 4. 해당 정보를 바탕으로 Tmdb를 검색하고, DB를 채움(save)
+
+        // 5. BoxOffice 역시 데이터를 채우고,
+
+        // 6. 데이터를 리턴한다.
     }
 }
