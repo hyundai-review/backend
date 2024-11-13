d72dd4fff2c6cec42adef79d0db93636a5a9ca35
Haneol <haneolj@gmail.com>
2024-11-13 20:07:58 +0900

    [feature/crawler] feat: 박스오피스 apiClient 구현

---
+++ src/main/java/hyundai/movie/domains/crawler/client/KobisApiClient.java
@@ -0,0 +1,34 @@
+package hyundai.movie.domains.crawler.client;
+
+import hyundai.movie.domains.crawler.dto.KobisBoxOfficeDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KobisApiClient {
+    private final WebClient webClient;
+
+    @Value("${kobis.api.key}")
+    private String apiKey;
+
+    @Value("${kobis.api.base-url}")
+    private String baseUrl;
+
+    // 일별 박스오피스
+    public KobisBoxOfficeDto getDailyBoxOffice(String targetDt) {
+        String requestUrl = baseUrl + "/boxoffice/searchDailyBoxOfficeList.json"
+                + "?key=" + apiKey
+                + "&targetDt=" + targetDt;
+
+        return webClient.get()
+                .uri(requestUrl)
+                .retrieve()
+                .bodyToMono(KobisBoxOfficeDto.class)
+                .block();
+    }
+}
