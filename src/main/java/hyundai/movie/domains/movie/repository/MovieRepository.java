909a399ceebed6282b83e09d7bfb21b26b46b8a0
Haneol <haneolj@gmail.com>
2024-11-15 01:51:17 +0900

    [feature/movie] feat: 영화 검색 기능 구현

--- src/main/java/hyundai/movie/domains/movie/repository/MovieRepository.java
+++ src/main/java/hyundai/movie/domains/movie/repository/MovieRepository.java
@@ -2,10 +2,17 @@ package hyundai.movie.domains.movie.repository;
 
 import hyundai.movie.domains.movie.domain.Movie;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findByTmdbId(Long tmdbId);
+
+    @Query("SELECT m FROM Movie m WHERE m.title LIKE %:keyword%")
+    Slice<Movie> searchByTitleContaining(@Param("keyword") String keyword, Pageable pageable);
 }
