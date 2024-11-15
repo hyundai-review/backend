2a255ed184ad533f54b74527ac3521dd590e4a85
zlnongi <yeossi8@naver.com>
2024-11-15 17:26:31 +0900

    [feature/comment] chore: 댓글에서 답글로 수정

--- src/main/java/hyundai/movie/domains/comment/api/request/CommentCreateRequest.java
+++ src/main/java/hyundai/movie/domains/comment/api/request/CommentCreateRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CommentCreateRequest {
-    @NotBlank(message = "댓글 내용은 필수 항목입니다.")
+    @NotBlank(message = "답글 내용은 필수 항목입니다.")
     private String content;
 
 }
