830b1550226fc7231802961346a84fc10ab3b75a
zlnongi <yeossi8@naver.com>
2024-11-17 20:51:47 +0900

    [feature/review] fix: 나의 리뷰에 isSpoil 확인 가능 기능 수정

--- src/main/java/hyundai/movie/domains/review/dto/ReviewforMyPageDto.java
+++ src/main/java/hyundai/movie/domains/review/dto/ReviewforMyPageDto.java
@@ -10,18 +10,20 @@ public class ReviewforMyPageDto {
     private final Long reviewId;
     private final Integer rating;
     private final String content;
+    private final Boolean isSpoil;
     private final String photocard;
     private final int totalComments;
     private final String createdAt;
     private final String updatedAt;
 
-    private ReviewforMyPageDto(Long movieId, String movieTitle, Long reviewId, Integer rating, String content,
+    private ReviewforMyPageDto(Long movieId, String movieTitle, Long reviewId, Integer rating, String content, Boolean isSpoil,
             String photocard, int totalComments, String createdAt, String updatedAt) {
         this.movieId = movieId;
         this.movieTitle = movieTitle;
         this.reviewId = reviewId;
         this.rating = rating;
         this.content = content;
+        this.isSpoil = isSpoil;
         this.photocard = photocard;
         this.totalComments = totalComments;
         this.createdAt = createdAt;
@@ -36,6 +38,7 @@ public class ReviewforMyPageDto {
                 review.getId(),
                 review.getRating(),
                 review.getContent(),
+                review.getIsSpoil(),
                 review.getPhotocard(),
                 totalComments,
                 review.getCreatedAt().toString(),
