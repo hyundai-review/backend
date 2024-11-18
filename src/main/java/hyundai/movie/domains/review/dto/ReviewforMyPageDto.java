c1196723ce16e9029a64a57403f434d85ffe7755
zlnongi <yeossi8@naver.com>
2024-11-18 12:00:28 +0900

    [feature/review] fix: 좋아요 기능 전체 조회리스트에 추가

--- src/main/java/hyundai/movie/domains/review/dto/ReviewforMyPageDto.java
+++ src/main/java/hyundai/movie/domains/review/dto/ReviewforMyPageDto.java
@@ -13,11 +13,12 @@ public class ReviewforMyPageDto {
     private final Boolean isSpoil;
     private final String photocard;
     private final int totalComments;
+    private final Boolean isLike;
     private final String createdAt;
     private final String updatedAt;
 
     private ReviewforMyPageDto(Long movieId, String movieTitle, Long reviewId, Integer rating, String content, Boolean isSpoil,
-            String photocard, int totalComments, String createdAt, String updatedAt) {
+            String photocard, int totalComments, Boolean isLike, String createdAt, String updatedAt) {
         this.movieId = movieId;
         this.movieTitle = movieTitle;
         this.reviewId = reviewId;
@@ -26,12 +27,13 @@ public class ReviewforMyPageDto {
         this.isSpoil = isSpoil;
         this.photocard = photocard;
         this.totalComments = totalComments;
+        this.isLike = isLike;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
 
     // 정팩메 ㅋㅋ
-    public static ReviewforMyPageDto from(Review review, int totalComments) {
+    public static ReviewforMyPageDto from(Review review, int totalComments, boolean isLike) {
         return new ReviewforMyPageDto(
                 review.getMovie().getId(),
                 review.getMovie().getTitle(),
@@ -41,6 +43,7 @@ public class ReviewforMyPageDto {
                 review.getIsSpoil(),
                 review.getPhotocard(),
                 totalComments,
+                isLike,
                 review.getCreatedAt().toString(),
                 review.getUpdatedAt().toString()
         );
--- src/main/java/hyundai/movie/domains/review/repository/ReviewLikeRepository.java
+++ src/main/java/hyundai/movie/domains/review/repository/ReviewLikeRepository.java
@@ -1,5 +1,7 @@
 package hyundai.movie.domains.review.repository;
 
+import hyundai.movie.domains.member.domain.Member;
+import hyundai.movie.domains.review.domain.Review;
 import hyundai.movie.domains.review.domain.ReviewLike;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +11,6 @@ import org.springframework.data.repository.query.Param;
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
     @Query("SELECT rl FROM ReviewLike rl JOIN FETCH rl.review r WHERE r.id = :reviewId AND rl.member.id = :memberId")
     Optional<ReviewLike> findReviewLikeByReviewIdAndMemberId(@Param("reviewId") Long reviewId, @Param("memberId") Long memberId);
+
+    boolean existsByReviewAndMember(Review review, Member member);
 }
